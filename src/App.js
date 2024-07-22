13e15e49d6ffeb5eb0cd57337d0731f1f3e61e0f
FowardPro <fowardnkuna6@gmail.com>
2024-07-22 14:50:46 +0200

    Changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit the <code>src/App.js</code> and save to reload.
+          Edit the gwg <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
